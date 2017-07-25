4ba2841bb1ce2d7d608e67bc1edde8b72c282a2c
liweng42 <liweng42@gmail.com>
2017-07-25 22:50:29 +0800

    生成16位唯一文件名

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,24 @@
 
 
 $(function(){
-	
+
 	var dropbox = $('#dropbox'),
 		message = $('.message', dropbox);
-	
+
 	dropbox.filedrop({
 		// The name of the $_FILES entry:
 		paramname:'pic',
-		
+
 		maxfiles: 5,
     	maxfilesize: 2,
 		url: 'post_file.php',
 		uploadFinished:function(i,file,response){
 			$.data(file).addClass('done');
 			// response is the JSON object that post_file.php returns
-            uploadSucceed(file.name);
+			result = JSON.parse(response)
+            uploadSucceed(result.filename);
 		},
-		
+
     	error: function(err, file) {
 			switch(err) {
 				case 'BrowserNotSupported':
@@ -33,28 +34,28 @@ $(function(){
 					break;
 			}
 		},
-		
+
 		// Called before each upload is started
 		beforeEach: function(file){
 			if(!file.type.match(/^image\//)){
 				alert('Only images are allowed!');
-				
+
 				// Returning false will cause the
 				// file to be rejected
 				return false;
 			}
 		},
-		
+
 		uploadStarted:function(i, file, len){
 			createImage(file);
 		},
-		
+
 		progressUpdated: function(i, file, progress) {
 			$.data(file).find('.progress').width(progress);
 		}
-    	 
+
 	});
-	
+
 	var template = '<div class="preview">'+
 						'<span class="imageHolder">'+
 							'<img />'+
@@ -63,37 +64,37 @@ $(function(){
 						'<div class="progressHolder">'+
 							'<div class="progress"></div>'+
 						'</div>'+
-					'</div>'; 
-	
-	
+					'</div>';
+
+
 	function createImage(file){
 
-		var preview = $(template), 
+		var preview = $(template),
 			image = $('img', preview);
-			
+
 		var reader = new FileReader();
-		
+
 		image.width = 100;
 		image.height = 100;
-		
+
 		reader.onload = function(e){
-			
+
 			// e.target.result holds the DataURL which
 			// can be used as a source of the image:
-			
+
 			image.attr('src',e.target.result);
 		};
-		
+
 		// Reading the file as a DataURL. When finished,
 		// this will trigger the onload function above:
 		reader.readAsDataURL(file);
-		
+
 		message.hide();
 		preview.appendTo(dropbox);
-		
+
 		// Associating a preview container
 		// with the file, using jQuery's $.data():
-		
+
 		$.data(file,preview);
 	}
 
